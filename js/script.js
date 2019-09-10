5e0a09a8610f27c73a87b82dbd1a8a72343df739
Przemek Drożniak <mr.shemeck@gmail.com>
2019-09-10 14:23:56 +0200

    FIX console logs, REMOVE comments and whitespace

--- js/script.js
+++ js/script.js
@@ -8,7 +8,6 @@ function playGame(playerInput) {
         } else if (argMoveId == 3) {
             return 'nożyce';
         }
-    
         printMessage('Nie znam ruchu o id ' + argMoveId + '.');
         return 'nieznany ruch';
     }
@@ -17,36 +16,14 @@ function playGame(playerInput) {
     console.log('Wylosowana liczba to: ' + randomNumber);
 
     let computerMove = getMoveName(randomNumber);
-
-    /*if (randomNumber == 1) {
-        computerMove = 'kamień';
-    } else if (randomNumber == 2) {
-        computerMove = 'papier';
-    } else if (randomNumber == 3) {
-        computerMove = 'nożyce';
-    }*/
-
     printMessage('Ruch komputera to: ' + computerMove);
 
-
-    //let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-    console.log('Gracz wpisał: ' + playerInput);
-
     let playerMove = getMoveName(playerInput);
-
-    /*if (playerInput == '1') {
-        playerMove = 'kamień';1
-    } else if (playerInput == '2') {
-        playerMove = 'papier';
-    } else if (playerInput == '3') {
-        playerMove = 'nożyce';
-    }*/
-
+    console.log('Gracz wpisał: ' + playerInput);
     printMessage('Twój ruch to: ' + playerMove);
 
-
     function displayResult(argComputerMove, argPlayerMove) {
-        console.log('moves:', argComputerMove, argPlayerMove);
+        console.log('Porównywane ruchy:', argComputerMove, argPlayerMove);
         if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
             printMessage('Ty wygrywasz!');
         } else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce') {
@@ -72,9 +49,7 @@ function playGame(playerInput) {
         if (argPlayerMove == 'nieznany ruch') {
             printMessage('Odśwież stronę i wpisz albo: "1", albo: "2", albo: "3"');
         }
-
     }
-
     displayResult(computerMove, playerMove);
 }
 
